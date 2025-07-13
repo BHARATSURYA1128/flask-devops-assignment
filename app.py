a1f47b29ca51daef9b4420b856f830391d254db2
surya <bharatsuryajithuga@gmail.com>
2025-07-13 15:11:12 +0530

    final commit for assignment

--- app.py
+++ app.py
@@ -1,24 +1,9 @@
-import bugsnag
-from bugsnag.flask import handle_exceptions
 from flask import Flask
-
-# Configure bugsnag
-bugsnag.configure(
-  api_key="1301d2fe80bc2664053777d009aaa0f1",
-  project_root="/path/to/your/app",
-)
-
-# Attach bugsnag to Flask's exception handler
 app = Flask(__name__)
-handle_exceptions(app)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask app with Bugsnag!"
-
-@app.route("/crash")
-def crash():
-    raise RuntimeError("This is a test error for Bugsnag!")
+    return "✅ Hello from Flask on Kubernetes via Docker Hub !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
